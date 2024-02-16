4f46714b77b97579fbe84af6d62f237de7d78bad
NovaHelmet <45287463+NovaHelmet@users.noreply.github.com>
2024-02-16 18:43:11 +0530

    Update app.py

--- Youtube/app.py
+++ Youtube/app.py
@@ -84,5 +84,3 @@ def extract_video_id(url):
     match = re.search(r'(youtu\.be/|youtube\.com/shorts/|youtube\.com/live/)([^&?/]+)', url)
     return match.group(2) if match else None
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
